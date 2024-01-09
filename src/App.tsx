89568170aae96f7d2788fa2369fd7ca02cc08684
gigokomrade apps <zetechuniversity254@gmail.com>
2024-01-09 23:05:58 +0000

    navbar and herosection creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-
-
-
 const App = () => {
+
   return (
     <div>
       <NavBar />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,16 +2,58 @@ import React from "react";
 
 import "./NavBar.css";
 
+import { GoHomeFill } from "react-icons/go";
+import { CiChat2 } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+
+
 const NavBar = () => {
   return (
     <>
       <div className="initial-gradient-backdrop"></div>
 
       <div className="nav-bar">
-        <h1 className="nav-bar-title">Kreative</h1>
+      <GoHomeFill className="home-icon"/> 
+     
+        <h1 className="nav-bar-title"> Kreative</h1>
+
+
+        <div className="nav-bar-right">
+
+        <CiChat2 className="chat-icon" />
+
+          <h1 className="chat-text">Chat</h1>
+          <FaRegUser className="user-icon" />
+          <h1 className="user-text">User</h1>
+
+        </div>
+       
+      </div>
+
+
+    
+
+      <div className="hero-section">
+
+        <div className="hero-section-left">
+        <span className="kreative-span">
+        Kreative ai- for
+          </span><br></br> 
+          <span className="coding-span">coding</span>
+          <span className="kreative-span">,
+           </span>
+          <span className="creation-span">content</span><br></br> <span className="creation-span">creation</span> 
+         <span className="kreative-span"> and </span>
+          <br></br> 
+          <span className="more-span">more</span>
+        </div>
+        <div className="hero-section-right"></div>
+
       </div>
     </>
   );
 };
 
 export default NavBar;
+
+
